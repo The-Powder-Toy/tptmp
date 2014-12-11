154823c9006a86224cd9b732ddbecdc3b4bab307
jacob1 <jfu614@gmail.com>
2014-12-10 20:05:38 -0500

    fix stalkchat hook clogging the socket

--- hooks/stalkchat.lua
+++ hooks/stalkchat.lua
@@ -6,24 +6,26 @@ end
 
 function serverHooks.stalkchat(client, cmd, msg)
 	if crackbot then
-		local output = ""
+		local output = nil
 		local room = client.room or "NONE"
 		local nick = client.nick or "NONE"
 		if cmd == 19 then
-			output = "\0308["..room.."]\03 <"..nick.."> "..msg
+			output = "08["..room.."] <"..nick.."> "..msg
 		elseif cmd == 20 then
-			output = "\0308["..room.."]\03 * "..nick.." "..msg
+			output = "08["..room.."] * "..nick.." "..msg
 		elseif cmd == 1 then
 			if room == "null" then
-				output = "\0311* "..nick.."["..client.host.."] has joined "..room
+				output = "11* "..nick.."["..client.host.."] has joined "..room
 			else
-				output = "\0311* "..nick.." has joined "..room
+				output = "11* "..nick.." has joined "..room
 			end
 			writeStatFile()
 		elseif cmd == -1 then
-			output = "\0305* "..nick.."["..client.host.."] has quit ("..msg..")"
+			output = "05* "..nick.."["..client.host.."] has quit ("..msg..")"
 			writeStatFile()
 		end
-		crackbot:send(output.."\n")
+		if output then
+			crackbot:send(output.."\n")
+		end
 	end
 end
