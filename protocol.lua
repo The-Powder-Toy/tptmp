1e88030ab642866a651bbbf3463cdd705d4bbb3b
cracker64 <cracker642@gmail.com>
2017-06-09 19:31:28 -0400

    Shorten protocol.lua

--- protocol.lua
+++ protocol.lua
@@ -5,261 +5,198 @@ elseif not bit then
 end
 local lshift, rshift, band, rol, ror = bit.lshift, bit.rshift, bit.band, bit.rol, bit.ror
 local insert, concat = table.insert, table.concat
-protocol = {
-	--Init_Connect
+protocol = { --The name of the protocol is removed later
 	[2] = {
+		"Init_Connect",
 		{name="major",size=1},
 		{name="minor",size=1},
 		{name="script",size=1},
 		{name="nick",size=1,str=true},
 	},
-	--Connect_Succ
-	[3] = {},
-	--Disconnect , Or could be a kick
+	[3] = {"Connect_Succ"},
 	[4] = {
+		"Disconnect",
 		{name="reason",size=1,str=true},
 	},
-	-- Ping
-	[8] = {},
-	-- Pong
-	[9] = {},
-	--New_Nick -- Server->Client, The client nick was changed
+	[8] = {"Ping"},
+	[9] = {"Pong"},
 	[13] = {
+		"New_Nick", -- Server->Client, The client nick was changed
 		{name="nick",size=1,str=true},
 	},
-	--Join_Chan, Try moving into new channel
 	[14] = {
+		"Join_Chan", -- Try moving into new channel
 		{name="chan",size=1,str=true},
 	},
-	--Chan_Name - Current_Channel, Response of Join_Chan (or a server forced move)
 	[15] = {
+		"Chan_Name", -- Current_Channel, Response of Join_Chan (or a server forced move)
 		{name="chan",size=1,str=true},
 	},
-	--Chan_Member -- Sent in response to join, One per user in channel, will fill a user list
 	[16] = {
+		"Chan_Member",-- Sent in response to join, One per user in channel, will fill a user list
 		{name="name",size=1,str=true},
 	},
-	--User_Join, User joins the channel
 	[17] = {
+		"User_Join", -- User joins the channel
 		{name="name",size=1,str=true},
 	},
-	--User_Leave, User leaves a channel
-	[18] = {},
-	--User_Chat, User chat
+	[18] = {"User_Leave"}, -- User leaves a channel
 	[19] = {
+		"User_Chat",
 		{name="msg",size=1,str=true},
 	},
-	--User_Me, User emote
 	[20] = {
+		"User_Me",
 		{name="msg",size=1,str=true},
 	},
-	-- User_Kick, kick a user
 	[21] = {
+		"User_Kick",
 		{name="nick",size=1,str=true},
 		{name="reason",size=1,str=true},
 	},
-	--Server_Broadcast, Colored message sent from server
 	[22] = {
+		"Server_Broadcast",
 		{name="msg",size=1,str=true},
 		{name="R",size=1},
 		{name="G",size=1},
 		{name="B",size=1},
 	},
-	--Set_User_Mode, Set some user modes (WIP)
 	[23] = {
+		"Set_User_Mode",
 		{name="nick",size=1,str=true},
 		{name="modes",size=1,bit=true,fields={"stab","mute","op"},sizes={1,1,1}},
 	},
-	--Get_User_Mode, Get some user modes (WIP)
 	[24] = {
+		"Get_User_Mode", -- Client->Server
 		{name="nick",size=1,str=true},
 	},
-	--User_Mode, Response from above (WIP)
 	[25] = {
+		"User_Mode", -- Server->Client
 		{name="nick",size=1,str=true},
 		{name="modes",size=1,bit=true,fields={"stab","mute","op"},sizes={1,1,1}},
 	},
-	--Mouse_Pos, Client mouse location
 	[32] = {
+		"Mouse_Pos",
 		{name="position",size=3,bit=true,fields={"x","y"},sizes={12,12}},
 	},
-	--Mouse_Click, Clicky at mouse location
 	[33] = {
+		"Mouse_Click",
 		{name="click",size=1,bit=true,fields={"button","event"},sizes={4,4}},
 	},
-	--Brush_Size
 	[34] = {
+		"Brush_Size",
 		{name="x",size=1},
 		{name="y",size=1},
 	},
-	--Brush_Shape,
 	[35] = {
+		"Brush_Shape",
 		{name="shape",size=1},
 	},
-	--Key_Mods, key: 0-ctrl 1-shift 2-alt
 	[36] = {
+		"Key_Mods", -- key: 0-ctrl 1-shift 2-alt
 		{name="key",size=1,bit=true,fields={"char","state"},sizes={4,4}},
 	},
-	--["Selected_Elem"] = 37,
 	[37] = {
+		"Selected_Elem",
 		{name="selected",size=2,bit=true,fields={"button","elem"},sizes={2,12}},
 	},
-	--["Replace_Mode"] = 38,
 	[38] = {
+		"Replace_Mode",
 		{name="replacemode",size=1},
 	},
-	--["Mouse_Reset"], Forced mouseup event due to zoom window or entering another interface
-	[39] = {},
-	--["View_Mode_Simple"] = 48,
+	[39] = {"Mouse_Reset"}, -- Forced mouseup event due to zoom window or entering another interface
 	[48] = {
+		"View_Mode_Simple",
 		{name="mode",size=1},
 	},
-	--["Pause_State"] = 49,
 	[49] = {
+		"Pause_State",
 		{name="state",size=1},
 	},
-	--["Frame_Step"] = 50,
-	[50] = {},
-	--["Deco_State"] = 51,
+	[50] = {"Frame_Step"},
 	[51] = {
+		"Deco_State",
 		{name="ID",size=1},
 		{name="state",size=1},
 	},
-	--["Ambient_State"] = 53,
 	[53] = {
+		"Ambient_State",
 		{name="state",size=1},
 	},
-	--["NGrav_State"] = 54,
 	[54] = {
+		"NGrav_State",
 		{name="state",size=1},
 	},
-	--["Heat_State"] = 56,
 	[56] = {
+		"Heat_State",
 		{name="state",size=1},
 	},
-	--["Equal_State"] = 57,
 	[57] = {
+		"Equal_State",
 		{name="state",size=1},
 	},
-	--["Grav_Mode"] = 58,
 	[58] = {
+		"Grav_Mode",
 		{name="state",size=1},
 	},
-	--["Air_Mode"] = 59,
 	[59] = {
+		"Air_Mode",
 		{name="state",size=1},
 	},
-	--["Clear_Spark"] = 60,
-	[60] = {},
-	--["Clear_Press"] = 61,
-	[61] = {},
-	--["Invert_Press"] = 62,
-	[62] = {},
-	--["Clear_Sim"] = 63,
-	[63] = {},
-	--["View_Mode_Advanced"] = 64,
+	[60] = {"Clear_Spark"},
+	[61] = {"Clear_Press"},
+	[62] = {"Invert_Press"},
+	[63] = {"Clear_Sim"},
 	[64] = {
+		"View_Mode_Advanced",
 		{name="display",size=1},
 		{name="render",size=1},
 		{name="color",size=1},
 	},
-	--["Selected_Deco"] = 65,
 	[65] = {
+		"Selected_Deco",
 		{name="RGBA",size=4},
 	},
-	--["Stamp_Data"] = 66,
 	[66] = {
+		"Stamp_Data",
 		{name="position",size=3,bit=true,fields={"x","y"},sizes={12,12}},
 		{name="data",size=3,str=true},
 	},
-	--["Clear_Area"] = 67,
 	[67] = {
+		"Clear_Area",
 		{name="start",size=3,bit=true,fields={"x","y"},sizes={12,12}},
 		{name="stop",size=3,bit=true,fields={"x","y"},sizes={12,12}},
 	},
-	--["Edge_Mode"] = 68,
 	[68] = {
+		"Edge_Mode",
 		{name="state",size=1},
 	},
-	--["Load_Save"] = 69,
 	[69] = {
+		"Load_Save",
 		{name="saveID",size=3},
 	},
-	--["Reload_Sim"] = 70,
-	[70] = {},
-	-- Sign_Data = 71,
+	[70] = {"Reload_Sim"},
 	[71] = {
+		"Sign_Data",
 		{name="signID",size=1},
 		{name="position",size=3,bit=true,fields={"x","y"},sizes={12,12}},
 		{name="text",size=1,str=true},
 		{name="just",size=1},
 	},
-	--["Req_Player_Sync"] = 128,  Server->Client Only
 	[128] = {
+		"Req_Player_Sync", -- Server->Client Only
 		{name="userID",size=1},
 	},
-	--Player_Sync, Client->Server only, packets unprocessed by the server, relayed to another client as original protocols
 	[129] = {
+		"Player_Sync", -- Client->Server only, packets unprocessed by the server, relayed to another client as original protocols
 		{name="userID",size=1},
 		{name="proto",size=1},
 		{name="data",size=3,str=true},
 	},
 }
-local protoName = {
-	["Init_Connect"] = 2,
-	["Connect_Succ"] = 3,
-	["Disconnect"] = 4,
-	["Ping"] = 8,
-	["Pong"] = 9,
-	["New_Nick"] = 13,
-	["Join_Chan"] = 14,
-	["Chan_Name"] = 15,
-	["Chan_Member"] = 16,
-	["User_Join"] = 17,
-	["User_Leave"] = 18,
-	["User_Chat"] = 19,
-	["User_Me"] = 20,
-	["User_Kick"] = 21,
-	["Server_Broadcast"] = 22,
-	["Set_User_Mode"] = 23,
-	["Get_User_Mode"] = 24,
-	["User_Mode"] = 25,
-	["Mouse_Pos"] = 32,
-	["Mouse_Click"] = 33,
-	["Brush_Size"] = 34,
-	["Brush_Shape"] = 35,
-	["Key_Mods"] = 36,
-	["Selected_Elem"] = 37,
-	["Replace_Mode"] = 38,
-	["Mouse_Reset"] = 39,
-	["View_Mode_Simple"] = 48,
-	["Pause_State"] = 49,
-	["Frame_Step"] = 50,
-	["Deco_State"] = 51,
-	["Ambient_State"] = 53,
-	["NGrav_State"] = 54,
-	["Heat_State"] = 56,
-	["Equal_State"] = 57,
-	["Grav_Mode"] = 58,
-	["Air_Mode"] = 59,
-	["Clear_Spark"] = 60,
-	["Clear_Press"] = 61,
-	["Invert_Press"] = 62,
-	["Clear_Sim"] = 63,
-	["View_Mode_Advanced"] = 64,
-	["Selected_Deco"] = 65,
-	["Stamp_Data"] = 66,
-	["Clear_Area"] = 67,
-	["Edge_Mode"] = 68,
-	["Load_Save"] = 69,
-	["Reload_Sim"] = 70,
-	["Sign_Data"] = 71,
-	["Req_Player_Sync"] = 128,
-	["Player_Sync"] = 129,
-}
---Put a reverse table in as well.
 protoNames = {}
-for k,v in pairs(protoName) do protoNames[k]=v protoNames[v]=k end
+--Create Name table and reverse table as well.
+for k,v in pairs(protocol) do local name=table.remove(v,1) protoNames[k]=name protoNames[name]=k end
 
 -- Protocol code --
 local function schar(s) if type(s)=="number" then return string.char(s) else return tostring(s) end end
