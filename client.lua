0983bba8d9c4503ea52f55e1d957804b5596b67a
jacob1 <jfu614@gmail.com>
2019-02-13 22:36:33 -0500

    version .91: make it not break on the beta / snapshots

--- client.lua
+++ client.lua
@@ -1,7 +1,7 @@
 --Cracker64's Powder Toy Multiplayer
 --I highly recommend to use my Autorun Script Manager
 
-local versionstring = "0.9"
+local versionstring = "0.91"
 
 --TODO's
 --FIGH,STKM,STK2,LIGH need a few more creation adjustments
@@ -316,8 +316,8 @@ new=function(x,y,w,h)
 	intext:moveadd(function(self,x,y) self.t:onmove(x,y) end)
 	function intext:setfocus(focus)
 		self.focus=focus
-		if focus then tpt.set_shortcuts(0) self:setcolor(255,255,0)
-		else tpt.set_shortcuts(1) self:setcolor(255,255,255) end
+		if focus then self:setcolor(255,255,0)
+		else self:setcolor(255,255,255) end
 	end
 	function intext:movecursor(amt)
 		self.cursor = self.cursor+amt
