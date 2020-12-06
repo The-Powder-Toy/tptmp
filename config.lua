df63e444fb6e9330fd6bdf701a1147838e4f6bf0
Tamás Bálint Misius <lbphacker@gmail.com>
2020-12-06 17:53:42 +0100

    Bump client version, make server +x

--- config.lua
+++ config.lua
@@ -3,8 +3,8 @@ config.bindhost="*"
 config.bindport=34403
 config.versionmajormin=91
 config.versionminormin=4
-config.versionmajormax=92
-config.versionminormax=5
-config.scriptversion=3
+config.versionmajormax=95
+config.versionminormax=0
+config.scriptversion=4
 config.pingtimeout=120
 return config
