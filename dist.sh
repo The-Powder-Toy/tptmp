33cc0c263cebdabc3f65c7fee71bbb8820526c1c
Tamás Bálint Misius <lbphacker@gmail.com>
2025-04-25 09:55:17 +0200

    Get modulepack.lua from the Script Manager
    By adding the Script Manager as a submodule.

--- dist.sh
+++ dist.sh
@@ -12,5 +12,5 @@ cd dist
 config_lua="$(grep -rn tptmp -e "local versionstr" | cut -d ":" -f 1)"
 real_version="$(echo "$1" | cut -d "/" -f 3-)"
 sed -i tptmp/client/config.lua -Ee 's/"v2\.[^"]+"/"'"$real_version"'"/'
-curl -fsS https://raw.githubusercontent.com/The-Powder-Toy/TPT-Script-Manager/refs/heads/master/modulepack.lua > modulepack.lua
-luajit modulepack.lua modulepack.conf > client.dist.lua
+git submodule update --init
+luajit ../TPT-Script-Manager/modulepack.lua modulepack.conf > client.dist.lua
