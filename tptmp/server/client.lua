The-Powder-Toy/tptmp

commit 8a90533b0bc041f331551f28f4b215f71d2a4afe

Fix crash when sending messages without the spam plugin enabled

tptmp/server/client.lua
@@ -201,8 +201,8 @@ function client_i:check_message_(message)
 		return
 	end
 	local server = self:server()
-	local ok
-	ok, message = server:phost():call_check_all("message_ok", self, message)
+	local ok, _
+	ok, _, message = server:phost():call_check_all("message_ok", self, message)
 	if not ok then
 		self:send_server("\ae* Cannot send message: " .. message)
 		return

tptmp/server/plugins/spam.lua
@@ -58,7 +58,7 @@ return {
 					end
 				end
 				client.spam_violations_ = nil
-				return "rewrite", message
+				return "rewrite", client, message
 			end,
 		},
 	},

commit 2cc7178674aee92f95833a9f112acf0c07feb172

Fix crash when message_ok hook reports failure
Broken by 8a90533b0bc0, where check_message_all's return values were scrambled.

tptmp/server/client.lua
@@ -201,10 +201,10 @@ function client_i:check_message_(message)
 		return
 	end
 	local server = self:server()
-	local ok, _
-	ok, _, message = server:phost():call_check_all("message_ok", self, message)
+	local ok, err
+	ok, err, message = server:phost():call_check_all("message_ok", self, message)
 	if not ok then
-		self:send_server("\ae* Cannot send message: " .. message)
+		self:send_server("\ae* Cannot send message: " .. err)
 		return
 	end
 	return message